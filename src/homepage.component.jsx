494fb8801f8da5e1d1f192e3f71ae2a26c231345
Thomas ZAWISZA <thomaszawisza@Thomass-MacBook-Pro.local>
2020-06-07 22:48:30 +0200

    Initial commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>HATS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>SNEAKERS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>JACKETS</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>WOMEN</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>MEN</h1>
+                    <span>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
