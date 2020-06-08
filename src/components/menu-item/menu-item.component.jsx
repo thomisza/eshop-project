ebef01d8560c2c2bb28733ae1e336d5b01f158d8
Thomas ZAWISZA <thomaszawisza@Thomass-MacBook-Pro.local>
2020-06-08 08:33:56 +0200

    added transition animation hover

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./menu-item.style.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{ backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item`}>
-    <div className="content">
-      <h1>{title}</h1>
-      <span>Shop Now</span>
-    </div>
+  <div className={`${size} menu-item`}>
+    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
   </div>
 );
 
