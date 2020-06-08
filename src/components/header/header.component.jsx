d86497860899d4935009f8a857b2aaed1f64bdfa
Thomas ZAWISZA <thomaszawisza@Thomass-MacBook-Pro.local>
2020-06-08 19:49:16 +0200

    added Header

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.style.scss'
+
+import { ReactComponent as Logo } from '../../assets/original.svg';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo'/>
+            </Link>
+            <div className='options'>
+                <Link className='option' to='/shop'>SHOP</Link>
+                <Link className='option' to='/shop'>CONTACT</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 
 import './homepage.style.scss';
-import MenuItem from '../../components/menu-item/menu-item.component';
 
 const HomePage = () => (
     <div className='homepage'>
