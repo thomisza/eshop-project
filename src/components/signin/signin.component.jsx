0e82d8915d9e5c4679b0be2ab0bb9667099beef1
Thomas ZAWISZA <thomaszawisza@Thomass-MacBook-Pro.local>
2020-06-10 08:02:23 +0200

    added sign in with email/pwd

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import "./signin.style.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util';
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+      password: ''
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({
-      email: "",
-      password: ""
-    });
+
+    const { email, password } = this.state;
+
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: ''
+      });
+    } catch(error){
+      console.error(error);
+    }
+
+    
   };
 
   handleChange = event => {
