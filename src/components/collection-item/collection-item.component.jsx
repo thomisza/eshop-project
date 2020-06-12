6f7f9093b057d7947edef2350122394ea9b1fec3
Thomas ZAWISZA <thomaszawisza@Thomass-MacBook-Pro.local>
2020-06-12 08:36:31 +0200

    able to add multiple products with same id

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./collection-item.style.scss";
+import CustomButton from "../custom-button/custom-button.component";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cart.action";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,8 +13,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}$</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./custom-button.style.scss";
 
-const CustomButton = ({children, isGoogleSignIn,  ...otherProps}) => {
-    return (
-    <button className={`${isGoogleSignIn ? 'google-signin' : ''} custom-button`} {...otherProps}>
-        {children}
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
+  return (
+    <button
+      className={`${inverted ? "inverted" : ""}  ${
+        isGoogleSignIn ? "google-signin" : ""
+      } custom-button`}
+      {...otherProps}
+    >
+      {children}
     </button>
-    );
-}
+  );
+};
 
 export default CustomButton;
